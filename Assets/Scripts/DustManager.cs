8979e8f300ad820a32c01cb05cd9b14c596d9538
sukapenpen <ne280194@senshu-u.jp>
2018-12-29 16:23:20 +0900

    ゴミが任意の個数落ちるようになった

---
+++ Assets/Scripts/DustManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DustManager : MonoBehaviour
+{
+	public GameObject DustPrefab;
+	public int DustCount = 10;
+	private GameObject[] dusts;
+	private int dustCount;
+
+	private void Awake()
+	{
+		dustCount = 0;
+		dusts = new GameObject[DustCount];
+		for (int i = 0; i < DustCount; i++)
+		{
+			dusts[i] = Instantiate(DustPrefab);
+			dusts[i].SetActive(false);
+		}
+	}
+
+	public void Start ()
+	{
+		StartCoroutine(AppearDust());
+		
+	}
+	
+	public void Update ()
+	{
+		
+	}
+
+	private IEnumerator AppearDust()
+	{
+		for (int i = 0; i < DustCount; i++)
+		{
+			dusts[i].SetActive(true);
+			yield return new WaitForSeconds(2.0f);
+		}
+	}
+}
