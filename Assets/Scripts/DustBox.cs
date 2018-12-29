b9f9cad564b1e17b939c248ec0f24759fa3d8b2a
sukapenpen <ne280194@senshu-u.jp>
2018-12-29 16:40:27 +0900

    とりあえずゴミ箱が動く

---
+++ Assets/Scripts/DustBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DustBox : MonoBehaviour
+{
+	private float speed = 2.0f;
+
+	public void Start ()
+	{
+		
+	}
+	
+	public void Update ()
+	{
+		if (Input.GetKey("up"))
+		{
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("down"))
+		{
+			transform.position -= transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("right"))
+		{
+			transform.position += transform.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("left"))
+		{
+			transform.position -= transform.right * speed * Time.deltaTime;
+		}
+	}
+}
