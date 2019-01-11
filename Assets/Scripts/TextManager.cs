e107e695a9fdff7c38bea854b1d52cbaaee0592d
sukapenpen <ne280194@senshu-u.jp>
2019-01-12 01:21:11 +0900

    ゴミが入るとカウントが増える関数の追加

---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : SingletonMonoBehaviour<TextManager>
+{
+	private GameObject gUIDustCounter;
+	private GameObject theNumberOfDusts;
+	private int dustCounter;
+
+	public void Init()
+	{
+		dustCounter = 0;
+		gUIDustCounter = GameObject.FindWithTag("DustCounter");
+		theNumberOfDusts = GameObject.FindWithTag("TheNumberOfDusts");
+		gUIDustCounter.GetComponent<TextMesh>().text = dustCounter.ToString();
+	}
+
+	public void AddDustCounter()
+	{
+		dustCounter += 1;
+		gUIDustCounter.GetComponent<TextMesh>().text = dustCounter.ToString();
+	}
+	
+}
